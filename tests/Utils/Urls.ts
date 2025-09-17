45218fea5d7995ab4fb6b9b186ead11d5dde3065
Ruchika99999 <ruchikakumari1999@gmail.com>
2025-09-17 19:55:33 +0530

    Complete registration part

--- tests/Utils/Urls.ts
+++ tests/Utils/Urls.ts
@@ -1,7 +1,7 @@
 // utils/urls.ts
 export const urls = {
   parabank: {
-    register: 'https://parabank.parasoft.com/register.htm',
+    register: 'https://parabank.parasoft.com/parabank/register.htm',
     
   },
  
