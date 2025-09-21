88bbb50b549da1cfa20e9cca4085a21f435f1804
Ruchika99999 <ruchikakumari1999@gmail.com>
2025-09-22 00:16:02 +0530

    Incorrect credentials

--- tests/OrangeHRMdata/logindata.ts
+++ tests/OrangeHRMdata/logindata.ts
@@ -2,5 +2,9 @@ export const logindata = {
   Login: {
     username: "Admin",
     password: "admin123"
+  },
+  Incorrect:{
+    username: "Admin",
+    password: "admin@123"
   }
 };
--- tests/orangeHRM.spec.ts
+++ tests/orangeHRM.spec.ts
@@ -10,21 +10,29 @@ test.describe('OrangeHRM Automate', () => {
     test.beforeAll(async () => { 
     browser = await chromium.launch(); 
     context = await browser.newContext(); 
-    page = await context.newPage(); 
+    page = await context.newPage();
+     
     }); 
-    test('Login Account', async () => { 
-        await page.goto(urls.LoginURL); 
-        const login = logindata.Login; 
-        
-        //Wait for username field to be visible and fill it 
-        await page.locator('input[name="username"]').fill(login.username); 
-        //Wait for password field and fill it 
-        await page.locator('input[name="password"]').fill(login.password);; 
+    test('ORM_LGN_FN_TC0001', async () => { 
         // Click login button 
         await page.getByRole('button', { name: 'Login' }).click(); 
-        
         //Assert dashboard is visible 
-        await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible(); }); 
+        await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible(); 
+    }); 
+    test('ORM_LGN_FN_TC0002',async()=>{
+         await page.goto(urls.LoginURL); 
+        const login1 = logindata.Incorrect; 
+        //Enter username  
+        await page.locator('input[name="username"]').fill(login1.username); 
+        //Enter password  
+        await page.locator('input[name="password"]').fill(login1.password);
+        // Click login button 
+        await page.getByRole('button', { name: 'Login' }).click(); 
+        //Validate the error message
+        const error= await page.getByText('Invalid credentials');
+        // 'CSRF token validation failed'
+        await expect(error).toHaveText('Invalid credentials')
+})
         
         test.afterAll(async () => { 
         await page.close(); 
